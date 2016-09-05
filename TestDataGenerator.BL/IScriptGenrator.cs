6c6ede394c27b553b0a8f0b1a17490d79871e99d
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2016-09-05 16:46:02 +0300

    Add project files.

---
+++ TestDataGenerator.BL/IScriptGenrator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDataGenerator.Data;
+
+namespace TestDataGenerator.BL
+{
+    public interface IScriptGenrator
+    {
+        UserEntity GenerateUser();
+        string GetValueLine(UserEntity entity);
+        string GetInsertLine();        
+        string CreateScript(int entityCount);
+    }
+}
